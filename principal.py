d670826af81a3f101d4365ac0ada83fe15ca44ca
PSepulveda88 <pablo.sepulveda@alumnos.frm.utn.edu.ar>
2023-05-14 19:03:19 -0300

    Reconocimiento registro y msjs

--- principal.py
+++ principal.py
@@ -4,8 +4,9 @@ from tkinter import *
 from tkinter import messagebox as msg
 import os
 from tkinter import ttk
-
-
+import cv2
+from mtcnn.mtcnn import MTCNN
+from matplotlib import pyplot as plt
 
 
 
@@ -31,6 +32,20 @@ color_error = "\033[1;31;40m"
 color_normal = "\033[0;37;40m"
 
 
+#GENERAL
+def saltoDeLinea(screen):
+    Label(screen, text="", bg=color_background).pack()
+    
+def imprimirMensaje(screen, text, flag):
+    if flag:
+        print(color_exito + text + color_normal)
+        screen.destroy()
+        msg.showinfo(message=text, title="¡Éxito!")
+    else:
+        print(color_error + text + color_normal)
+        Label(screen, text=text, fg="red", bg=color_background, font=(font_label, 12)).pack()
+
+
 def configurarPantalla(screen, text, image_screen):
     screen.title(text)
     screen.geometry(size_screen)
@@ -44,9 +59,37 @@ def configurarEntradaDatos(screen, var, flag):
     entry = Entry(screen, textvariable=var, justify=CENTER, font=(font_label, 12))
     entry.focus_force()
     entry.pack(side=TOP, ipadx=30, ipady=6)
+    
+    saltoDeLinea(root)
     frame = Frame(screen)
     frame.pack()
+    
+    if flag == 0:
+        Button(frame, text="Registrarse", fg=color_blanco, bg=color_negro, borderwidth=10,font=(font_label, 14), height="2", width="40", command=capturarRostroRegistro).grid(row=5, column=0, padx=0, pady=0, sticky="nsew")
+    
+    return entry
 
+#REGISTRO 
+def capturarRostroRegistro():
+    cap = cv2.VideoCapture(0)
+    usuario_reg_img = usuario1.get()
+    img = f"{usuario_reg_img}.jpg"
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Registro Facial", frame)
+        if cv2.waitKey(0) == 27:
+            break
+    
+    cv2.imwrite(img, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+    usuario_entry1.delete(0, END)
+    
+    pixeles = plt.imread(img)
+    faces = MTCNN().detect_faces(pixeles)
+        
 def ventanaRegistro():
     global usuario1
     global usuario_entry1
@@ -84,10 +127,10 @@ Label(text="¡Bienvenido!", fg=color_blanco, bg=color_negro, font=(font_label, 2
 Label(text="¿Seras digno del ingreso?", fg=color_blanco, bg=color_negro, font=(font_label, 20), width="500", height="2").pack() 
 
 #Botones de ingreso y registro
-
+saltoDeLinea(root)
 Button(text=txt_ingreso, fg=color_blanco, bg=color_negro_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40").pack()
 
-
+saltoDeLinea(root)
 Button(text=txt_registro, fg=color_blanco, bg=color_negro_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=ventanaRegistro).pack()
 
 
