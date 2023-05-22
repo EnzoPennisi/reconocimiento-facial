807f9331e9554f0cbb23eac783d8c2302d0af5a7
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2023-05-22 20:10:28 -0300

    Compatibilidad

--- principal.py
+++ principal.py
@@ -141,7 +141,22 @@ def ventanaRegistro():
     configurarPantalla(screen1, txt_registro, bg_img_registro)
     usuario_entry1 = configurarEntradaDatos(screen1, usuario1, 0)
     
-# INGRESO 
+# INGRESO
+def compatibilidad(img1, img2):
+    orb = cv2.ORB_create()
+
+    kpa, dac1 = orb.detectAndCompute(img1, None)
+    kpa, dac2 = orb.detectAndCompute(img2, None)
+
+    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = comp.match(dac1, dac2)
+
+    similar = [x for x in matches if x.distance < 70]
+    if len(matches) == 0:
+        return 0
+    return len(similar)/len(matches)
+
 def ventanaIngreso():
     global screen2
     global usuario2
